4a09f92564a390a3df3d2a8389142514f3266829
Shabnam <sbeiraghian@my.bcit.ca>
2025-12-19 00:52:33 -0800

    openAI added

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("API_KEY"))
 
 app = FastAPI()
 
@@ -15,10 +22,16 @@ app.add_middleware(
 class ChatRequest(BaseModel):
     text: str
 
-@app.get("/health")
-def health():
-    return {"ok": True}
+# @app.get("/health")
+# def health():
+#     return {"ok": True}
 
 @app.post("/chat")
 def chat(body: ChatRequest):
-    return {"replay": f"You said: {body.text}"}
+    response = client.responses.create(
+        model="gpt-5-nano",
+        input=body.text,
+    )
+    return {
+        "reply": response.output_text
+    }
